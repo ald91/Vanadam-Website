b7444d0b6c71aa8d7b97c8ca3c3fcf3646d92f24
Adam Davis <skaterdavis21@gmail.com>
2025-10-28 13:32:46 +0000

    Converted site to flask framework

---
+++ app.py
@@ -0,0 +1,28 @@
+# This code imports the Flask library and some functions from it.
+from flask import Flask, render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Routes
+#===================
+# These define which template is loaded, or action is taken, depending on the URL requested
+#===================
+# Home Page
+@app.route('/')
+def index():
+    return render_template('home.html', title="Vanadam Halo")
+
+@app.route('/mapPage/<mapName>', methods=['GET'])
+def mapPage(mapName):
+    print(f'got request for: {mapName}')
+    return render_template(f'{mapName}.html', mapName=mapName)
+
+# Run application
+#=========================================================
+# This code executes when the script is run directly.
+if __name__ == '__main__':
+    print("Starting Flask application...")
+    print("Open Your Application in Your Browser: http://localhost:81")
+    # The app will run on port 81, accessible from any local IP address
+    app.run(host='0.0.0.0', port=81)
