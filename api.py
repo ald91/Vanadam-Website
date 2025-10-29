77db4e7c6afe63b8777b2956a8d8e579dbb14f8c
“c5031264” <c5031264@shu.hallam.ac.uk>
2025-10-29 11:30:31 +0000

    assets added to git and SQL started

---
+++ api.py
@@ -0,0 +1,32 @@
+import requests
+from core import Keys
+
+API_KEY = Keys["API_KEY"]
+UPLOADS_PLAYLIST_ID = "UU4wPP_aSG0kR924KKE2OGWQ"  # Your channel's uploads playlist
+
+videos = []
+url = "https://www.googleapis.com/youtube/v3/playlistItems"
+params = {
+    "part": "snippet",
+    "playlistId": UPLOADS_PLAYLIST_ID,
+    "maxResults": 50,
+    "key": API_KEY
+}
+
+while True:
+    response = requests.get(url, params=params)
+    data = response.json()
+    
+    for item in data.get("items", []):
+        video_id = item["snippet"]["resourceId"]["videoId"]
+        title = item["snippet"]["title"]
+        videos.append({"id": video_id, "title": title})
+
+    if "nextPageToken" in data:
+        params["pageToken"] = data["nextPageToken"]
+    else:
+        break
+
+print(f"Found {len(videos)} videos:")
+for v in videos:
+    print(v["title"], v["id"])
