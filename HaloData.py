689d6d3828aae9906fd54bb55a4d3219b05c81e3
c5031264 <c5031264@hallam.shu.ac.uk>
2025-11-17 11:48:22 +0000

    small modifications + Fixes. Halo Data now ready for use (Halo infinite only)

--- HaloData.py
+++ HaloData.py
@@ -1,3 +1,19 @@
+
+
+#easy modification of website data without pissing about with back end?
+
+#on boot from JSON
+def Build_Halo_Data(form, credentials):
+    pass
+
+# modify JSON (CLASS???)
+def Modify_Hall_Data(form, credentials):
+    pass
+
+
+#Data stored by game
+#can edit using admin panel
+
 HI_MODES = {
     0 : "Not in rotation",
     1 : "Strongholds",
@@ -8,94 +24,156 @@ HI_MODES = {
     6 : "Assault"
 }
 
-HI_MAPS = {
-    "Aquarius": {
-        "name": "Aquarius",
-        "description": "Aquarius Terraforming Solutions: Industry leader in returning life to barren worlds.",
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
-    
-    "Bazaar": {
-        "name": "Bazaar",
-        "description": "Dusty streets and double doors.",
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+HALO_3_DATA = {
+    "game" : "Halo 3",
+    "maps" : { 
+        "placeholder": {
+            "name": None,
+            "description": None,
+            "ranked arena" : None,
+            "MM modes": None,
+        },
 
-    "Livefire": {
-        "name": "Live Fire",
-        "description": "Instructors at the Avery J. Johnson Academy of Military Science ensure their Spartans are prepared for any challenge.",
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+    }
+}
 
-    
-    "Recharge": {
-        "name": "Recharge",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+HALO_INFINITE_DATA = {
+    "Game" : "Halo:Infinite",
+    "Maps" :  {
+        "Aquarius": {
+            "name": "Aquarius",
+            "description": "Aquarius Terraforming Solutions: Industry leader in returning life to barren worlds.",
+            "ranked arena" : True,
+            "MM modes": "3 5 6",
+            "HCS modes": "3 5 6",
+            "EHL modes": "3 5 6"
+        },
+        
+        "Bazaar": {
+            "name": "Bazaar",
+            "description": "Dusty streets and double doors.",
+            "ranked arena" : False,
+            "MM modes": "0",
+            "HCS modes": "0",
+            "EHL modes": "0"
+        },
 
-    "Forbidden": {
-        "name": "Recharge",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+        "Livefire": {
+            "name": "Live Fire",
+            "description": "Instructors at the Avery J. Johnson Academy of Military Science ensure their Spartans are prepared for any challenge.",
+            "ranked arena" : True,
+            "MM modes": "1 2 4 5",
+            "HCS modes": "1 2 4 5",
+            "EHL modes": "1 2 4 5"
+        },
 
-    "Empyrean": {
-        "name": "Empyrean",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
-    
-    "Lattice": {
-        "name": "lattice",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+        
+        "Recharge": {
+            "name": "Recharge",
+            "description": "Power still courses through the walls of this neglected Axys facility.",
+            "ranked arena" : True,
+            "MM modes": "1 2 4 5",
+            "HCS modes": "1 2 4 5",
+            "EHL modes": "1 2 4 5"
+        },
 
-    "Origin": {
-        "name": "Origin",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+        "Forbidden": {
+            "name": "Recharge",
+            "description": "These halls hide history of both triumph and terror.",
+            "ranked arena" : True,
+            "MM modes": "3",
+            "HCS modes": "3",
+            "EHL modes": "3"
+            
+        },
+        "Fortress": {
+            "name": "Fortress",
+            "description": "Battles of long ago echo throughout the valley.",
+            "ranked arena" : True,
+            "MM modes": " 3 6 ",
+            "HCS modes": "3 6 ",
+            "EHL modes": "3 6"
+            
+        },
 
-    "Serenity": {
-        "name": "Serenity",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+        "Empyrean": {
+            "name": "Empyrean",
+            "description": "Sleepless nights, timeless fights.",
+            "ranked arena" : True,
+            "MM modes": "5",
+            "HCS modes": " 0 ",
+            "EHL modes": " 0 "
+        },
+        
+        "Lattice": {
+            "name": "Lattice",
+            "description": "This abandoned hydroelectric facility holds secrets once buried under the ice.",
+            "ranked arena" : True,
+            "MM modes": "1 2 4 5",
+            "HCS modes": "1 2 4 5"
+        },
 
-    "Solitude": {
-        "name": "Solitude",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
-    },
+        "Origin": {
+            "name": "Origin",
+            "description": "Forerunner Warrior-Servants tested themselves in this arena before facing the Flood. Remake of Halo 5 Coliseum. Plays 4v4 CTF, Slayer, Strongholds, Oddball, King of the Hill, and Free For All",
+            "ranked arena" : True,
+            "MM modes": "3 5",
+            "HCS modes": "3 5",
+            "EHL modes": "3 5"
+        },
 
-    "Streets": {
-        "name": "Streets",
-        "description": None,
-        "ranked arena" : True,
-        "MM modes": None,
-        "HCS modes": None
+        "Serenity": {
+            "name": "Serenity",
+            "description": "Amidst ancient ruins, tranquil waters flow through a symmetrical landscape. This Sanctuary, once sacred, now echoes with battle cries",
+            "ranked arena" : True,
+            "MM modes": " 3 6 ",
+            "HCS modes": " 0 ",
+            "EHL modes": " 0 "
+        },
+
+        "Solitude": {
+            "name": "Solitude",
+            "description": "Not all life finds a way.",
+            "ranked arena" : True,
+            "MM modes": " 2 5 ",
+            "HCS modes": " 5 ",
+            "EHL modes": " 2 5 "
+        },
+
+        "Streets": {
+            "name": "Streets",
+            "description": "The tranquility of this Mombasa back alley is pierced by the sound of heavy gunfire.",
+            "ranked arena" : True,
+            "MM modes": " 4 5 ",
+            "HCS modes": " 5 ",
+            "EHL modes": " 4 5 "
+        },
+
+        "Interference": {
+            "name" : "Interferance",
+            "description": "Reimagination of the Halo 5's map The Rig with a snowy theme. Upscaled to accommodate Infinite's mechanics",
+            "ranked arena" : False,
+            "MM modes": " 0 ",
+            "HCS modes": " 0 ",
+            "EHL modes": " 0 "
+        },
+
+        "Starboard": {
+            "name" : "Starboard",
+            "description": "General quarters, general quarters. All hands man your battle stations.",
+            "ranked arena" : False,
+            "MM modes": " 0",
+            "HCS modes": " 0 ",
+            "EHL modes": " 0 "
+        },
+
+        "Inquisitor" : {
+            "name": "inquisitor",
+            "description" : "Don't let its luxury fool you the Pious Inquisitor is one of the fastest ships in the Covenant fleet.",
+            "ranked arena" : False,
+            "MM modes": " 0",
+            "HCS modes": " 0 ",
+            "EHL modes": " 0 "
+        }
     },
-    # Add more maps here...
 }
