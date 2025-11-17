e16bc3ebda07d81379f84638eeff66eedd05092c
ald91 <skaterdavis21@gmail.com>
2025-11-17 16:13:41 +0000

    pagination fixed, some issues with map detection. YT need standardizing

--- api.py
+++ api.py
@@ -6,9 +6,14 @@ import isodate
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 #internal imports
 from HaloData import HALO_INFINITE_DATA
-KEY = os.getenv("GOOGLE_API_KEY", )
+KEY = os.getenv("GOOGLE_API_KEY", "ERROR")
+DIR = os.path.dirname(os.path.abspath(__file__))
+DATA = os.path.join(DIR, "API_SCRAPE.JSON")
 
 UPLOADS_PLAYLIST_ID = "UU4wPP_aSG0kR924KKE2OGWQ"  # Your channel's uploads playlist
 
@@ -61,13 +66,12 @@ def initVideoLibaray():
             mode = ["king of the hill", "koth", "slayer", "oddball", "assault", "ctf", "capture the flag"]
             #maps?
             
-            description = description.lower()
-    
-            video_rank = next((vr for vr in rank if vr.lower() in description), "NO RANK DETECTED") 
-            video_gameMode = next((gm for gm in mode if gm.lower() in description), "NO GAMEMODE DETECTED")
-            video_map = None
-            
-            #next((m for m in HALO_INFINITE_DATA.get("maps") if m.lower() in description), "NO MAP DETECTED")
+            words = description.lower().split(" ")
+            desc = description.lower()
+
+            video_rank = next((vr for vr in rank if vr.lower() in words), "NO RANK DETECTED") 
+            video_gameMode = next((gm for gm in mode if gm in words), "NO GAMEMODE DETECTED")
+            video_map = next((m for m in HALO_INFINITE_DATA.get("Maps", {}).keys() if m.lower() in desc), "NO MAP DETECTED")
 
             #not yet implemented (unsure of logic)
             video_category = None
@@ -85,7 +89,9 @@ def initVideoLibaray():
                 #handled later
                 "type": None,
                 "duration seconds": None,
-                "category": None})
+                "category": None,
+                "description": description
+                })
             
         next_page_token = data.get("nextPageToken")
 
@@ -106,52 +112,55 @@ def initVideoLibaray():
         #build dictionary of IDs from earlier
     
     video_lookup = {video["id"]: video for video in videos}
-    
-    url = "https://www.googleapis.com/youtube/v3/videos"
-    params = {
-        "part": "snippet,contentDetails,liveStreamingDetails",
-        "id": ",".join(video_lookup.keys()),
-        "key": KEY
-    }
+    video_ids = list(video_lookup.keys())
 
-    response = requests.get(url, params=params)
-    data = response.json()
-
-
-    for item in data.get("items", []):
-        vid_id = item["id"]
-        video = video_lookup[vid_id] 
-        description = item.get("snippet", {}).get("description", "").lower()
-
-
-        #duration checks
-        if "contentDetails" in item:
-            duration_ISO = item["contentDetails"]["duration"]
-            video["duration seconds"] = int(isodate.parse_duration(duration_ISO).total_seconds())
-        else:
-            video["duration seconds"] = None
-
-        #check if the video is or was a livestream, livestreams that have happened always have a finish time in the snippet[""]
-        if "this was a livestream" in description.lower():
-            video["video type"] = "livestream"
-            video["rank"] = None
-            video["subrank"] = None
-
-        elif type( video["duration seconds"]) == int and  video["duration seconds"] <= 60:
-            video["video type"] = "shortform"
-
-        elif type( video["duration seconds"]) == int and  video["duration seconds"] >= 61:
-            video["video type"] = "longform"
+    for i in range(0,len(video_ids),50):
+        chunk = video_ids[i:i+50]
         
-        else:
-            video["video type"] = None
+        url = "https://www.googleapis.com/youtube/v3/videos"
+        params = {
+            "part": "snippet,contentDetails,liveStreamingDetails",
+            "id": ",".join(chunk),
+            "key": KEY
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+
+        for item in data.get("items", []):
+            vid_id = item["id"]
+            video = video_lookup[vid_id] 
+            description = item.get("snippet", {}).get("description", "").lower()
+
+
+            #duration checks
+            if "contentDetails" in item:
+                duration_ISO = item["contentDetails"]["duration"]
+                video["duration seconds"] = int(isodate.parse_duration(duration_ISO).total_seconds())
+            else:
+                video["duration seconds"] = None
+
+            #check if the video is or was a livestream, livestreams that have happened always have a finish time in the snippet[""]
+            if "this was a livestream" in description.lower():
+                video["video type"] = "livestream"
+                video["rank"] = None
+                video["subrank"] = None
+                video["map"] = None
+
+            elif type( video["duration seconds"]) == int and  video["duration seconds"] <= 60:
+                video["video type"] = "shortform"
+
+            elif type( video["duration seconds"]) == int and  video["duration seconds"] >= 61:
+                video["video type"] = "longform"
+            
+            else:
+                video["video type"] = None
 
     if "nextPageToken" in data:
         params["pageToken"] = data["nextPageToken"]
     else:
         pass
-
-
 
     with open ('videos.json', 'w', encoding="utf-8") as file:
         json.dump(videos, file, indent=4, ensure_ascii=False)
@@ -189,5 +198,5 @@ def initThumnailLibrary():
 initVideoLibaray()
 print ("successfully initialised Video Library JSON")
 
-initThumnailLibrary()
+#initThumnailLibrary()
 print ("successfully initialised thumbnail Library JPEGS")
