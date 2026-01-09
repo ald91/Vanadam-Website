41c8322fc2d29d4308a68ddb931c3e4d8c2e443b
notacat1221 <111917734+notacat1221@users.noreply.github.com>
2026-01-09 18:13:46 +0000

    Updated forum\routes
    annotation, small bug fixes

--- app/forum/routes.py
+++ app/forum/routes.py
@@ -1,6 +1,5 @@
-
 #external modules
-from flask import render_template, redirect, url_for, request, session
+from flask import render_template, redirect, url_for, request, session, flash
 
 
 #python modules
@@ -21,7 +20,7 @@ from ..data.db import get_database
 #Admin dashboard
 @forum.route('/', methods=["GET", "POST"])
 def forum_page():
-    """ forum entry point """
+    """ Display current 'Looking for Group' (LFG) posts """
     if request.method == "GET":
         posts = fetch_all()
     
@@ -30,9 +29,21 @@ def forum_page():
 
     return render_template("all_forums.html", posts=posts)
 
+@forum.route('/<id>/view', methods=["GET"])
+def view_post(id):
+    """View an individual post in more detail"""
+    post = fetch_one(id)
+    comments = fetch_comments(id)
+    comment_form = CommentForm()
+    current_user = session.get('username')
+    report_form = ReportForm()
+
+    return render_template("view_post.html", post=post, comment=comment_form, comments=comments, current_user=current_user, report_form=report_form)
+
 @forum.route('/create', methods=["GET", "POST"])
 @login_required
 def create_post():
+    """Allows users to create LFG posts to find other users to play with"""
     form = ForumForm()
     username = session['username']
 
@@ -46,6 +57,7 @@ def create_post():
 @forum.route('/<id>/comment', methods=["GET", "POST"])
 @login_required
 def create_comment(id):
+    """Comment on other users' LFG posts"""
     form = CommentForm()
     username = session.get('username')
 
@@ -55,36 +67,39 @@ def create_comment(id):
 
     return redirect(url_for("forum.view_post", id=id, username=username))
 
-@forum.route('/<id>/view', methods=["GET"])
-def view_post(id):
-    post = fetch_one(id)
-    comments = fetch_comments(id)
-    comment_form = CommentForm()
-    current_user = session.get('username')
-    report_form = ReportForm()
-
-    return render_template("view_post.html", post=post, comment=comment_form, comments=comments, current_user=current_user, report_form=report_form)
-
 @forum.route('/<id>/edit', methods=["GET", "POST"])
+@login_required
 def edit_post(id):
-    form = ForumForm()
+    """Allows for users to edit their own submission"""
+    try:
+        form = ForumForm()
 
-    if not form.validate_on_submit():
-        form = prefill(form, id)
+        # Prefill the input boxes with the pre-existing content
+        if not form.validate_on_submit():
+            form = prefill(form, id)
 
-    if form.validate_on_submit():
-        modify(form, id)
+        # Submit and write to db
+        if form.validate_on_submit():
+            modify(form, id)
+            return redirect((url_for("forum.view_post", id=id, username=session['username'])))
 
-    return render_template("edit_post.html", form=form)
+        return render_template("edit_post.html", form=form)
+
+    except:
+        print("Form not valid")
+        flash("Post not found", "error")
+        return redirect(url_for("forum.forum_page"))
 
 @forum.route('/<id>/delete', methods=["GET", "DELETE"])
 def delete_post(id):
+    """For users to delete their own submission"""
     delete_post_record(id)
     return redirect(url_for("forum.forum_page"))
 
 @forum.route('/<id>/report', methods=["GET", "POST"])
 @login_required
 def report(id):
+    """Allows a user to report other users' submissions, cannot report same content twice"""
     form = ReportForm()
     current_user = session.get('username')
     form.target_id.data = id
