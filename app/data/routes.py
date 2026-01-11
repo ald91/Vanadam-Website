90cdaffd0a75626d0bde7fe1a2f1275ebf49b91c
Adam Davis <skaterdavis21@gmail.com>
2026-01-11 16:18:22 +0000

    delint

--- app/data/routes.py
+++ app/data/routes.py
@@ -10,19 +10,16 @@ from . import data
 
 
 @data.route('/articles_img/<filename>')
-def Data_Send_Article_IMG(filename: str) -> str:
-    
+def data_send_article_img(filename: str) -> str:
     """ returns a file path for article images x.jpeg. App cannot return files outside of static, this keeps data 
-     in data so article files are easier to manage """
-    
+    in data so article files are easier to manage """
     folder_path = os.path.join(current_app.root_path, "Data/Articles/ArticlesIMG")
 
     return send_from_directory(folder_path, filename)
 
+#TODO: NYI do after submissions
 @data.route('/coaching/<filename>')
-def Data_Send_Coaching_JSON(filename: str) -> dict:
+def data_send_coaching_JSON(filename: str) -> dict:
 
-    """ looks in coaching dir and returns associated JSON file with fields TODO"""
-
-    pass
+    """ NYI looks in coaching dir and returns associated JSON file with fields"""
 
