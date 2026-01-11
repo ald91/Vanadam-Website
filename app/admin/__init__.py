1878e1aac083a4d399ae9e88970457a64a884cdb
Adam Davis <skaterdavis21@gmail.com>
2026-01-11 14:50:11 +0000

    test files

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint
 
+admin = Blueprint("admin", __name__, template_folder="templates/admin")
+
 # routes.py import
 from . import routes
-
-admin = Blueprint("admin", __name__, template_folder="templates/admin")
